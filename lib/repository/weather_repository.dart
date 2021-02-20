e0f95c7cdccf3bd5e1c3dce50875bf9f8d64f6c4
Nephthalim Abebe <nephthalimabebe@Nephthalims-MacBook-Pro.local>
2021-02-20 16:31:35 -0500

    Fixed Repository

--- lib/repository/weather_repository.dart
+++ lib/repository/weather_repository.dart
@@ -3,10 +3,10 @@ import 'package:weather_app/data_provider/weather_api.dart';
 import 'package:weather_app/model/weather.dart';
 
 class WeatherRepository {
-  final WeatherApi _weatherRepository = WeatherApi();
+  final WeatherApi _weatherApi = WeatherApi();
 
   Future getWeatherForLocation(String cityName) async {
-    final rawWeather = await _weatherRepository.getRawWeather(cityName);
+    final rawWeather = await _weatherApi.getRawWeather(cityName);
     final Weather weather = Weather.parseJson(rawWeather);
 
     return weather;
